1f1b5274d7106537b30e028f4a25d4223891fb49
“JASON” <jrcorey84@gmail.com>
2017-02-07 07:10:19 -0600

    completed most of the problems

---
+++ javascriptReps.js
@@ -0,0 +1,85 @@
+// Round 1
+var strings = []
+var lengths = function(arrayOfStrings){
+	for(var i = 0; i < arrayOfStrings.length; i++){
+		strings.push(arrayOfStrings[i].length);
+	}
+
+	console.log(strings);
+}
+
+// Round 2
+
+var transmogrifier = function(a,b,c){
+	return Math.pow(a*b,c);
+}
+
+// Round 3
+
+var toonify = function(accent,sentence){
+	if(accent === "daffy"){
+		var newSentence = sentence.replace(/s/gi,"th");
+	}
+	else if(accent === "elmer"){
+		var newSentence = sentence.replace(/r/gi,"w");
+	}
+
+	else {
+		console.log(sentence);
+	}
+	
+	console.log(newSentence);
+}
+
+//Round 4
+
+var wordReverse = function(string){
+	var strSplit = string.split(" ");
+		var reverseStr = strSplit.reverse().join(' ');
+				
+			console.log(reverseStr);
+		}
+
+//Round 5
+
+var letterReverse = function(string){
+	var splitStr = string.split("");
+		var newStr = splitStr.reverse().join('');
+			console.log(newStr);
+	}
+
+//Round 6 CAN'T FIGURE OUT!
+var longestWord = function(strArray){
+	var longest = 0;
+		for(i=0; i < strArray.length; i++){
+			if(longest < strArray[i].length){
+			longest = strArray[i].length;
+			
+				console.log(longest)
+			}
+		}
+	}
+
+
+//Round 7
+
+
+var repMaster = function(input,wordReverse){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
